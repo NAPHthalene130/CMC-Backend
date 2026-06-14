f36f6443be62b5eeda0d9693fbd078795a6a366b
NAPHthalene130 <1301537275@qq.com>
2026-06-14 14:50:31 +0800

    fix(export): 日志导出Excel时间列添加列宽和日期格式注解修复#####显示

--- src/main/java/com/cmc/entity/Log.java
+++ src/main/java/com/cmc/entity/Log.java
@@ -1,5 +1,7 @@
 package com.cmc.entity;
 
+import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -12,6 +14,8 @@ public class Log {
     private Long userId;
     private String username;
     private String content;
+    @ColumnWidth(22)
+    @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
     private LocalDateTime time;
     private String ip;
     private String type;
